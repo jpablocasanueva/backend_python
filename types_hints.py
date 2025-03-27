c0735e613b3915755bf01abc0a12d7b958b92775
jpcasnauevab <jpcasnauevab@outlook.com>
2025-03-26 21:24:02 -0300

    types_hints

---
+++ types_hints.py
@@ -0,0 +1,18 @@
+### Type Hints
+
+my_string = "Hola Mundo"
+print(my_string)
+print(type(my_string))
+
+my_string = 22
+print(my_string)
+print(type(my_string))
+
+my_typed_variable: str = "Mi tipado es cadena de texto"
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
