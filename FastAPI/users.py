d96c00d1d2dff403628fc74357056fdb5e669543
jpcasanuevab <jpcasanuevab@outlook.com>
2025-03-30 22:29:46 -0300

    FIn Clase GET

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -4,14 +4,15 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
-users_list = [User(name= 'Bruno', surname= 'Casanueva', url= 'https://bruno.casanueva.com', age= 8),
-                User(name= 'Luna', surname= 'Casanueva', url= 'https://luna.casanueva.com', age= 9),
-                User(name= 'Oso', surname= 'Casanueva', url= 'https://oso.casanueva.com', age= 3)]
+users_list = [User(id=1,name= 'Bruno', surname= 'Casanueva', url= 'https://bruno.casanueva.com', age= 8),
+                User(id=2,name= 'Luna', surname= 'Casanueva', url= 'https://luna.casanueva.com', age= 9),
+                User(id=3,name= 'Oso', surname= 'Casanueva', url= 'https://oso.casanueva.com', age= 3)]
 
 @app.get('/usersJson')
 async def users():
@@ -22,3 +23,20 @@ async def users():
 @app.get('/userClass')
 async def usersClass():
     return users_list
+
+#Path
+@app.get('/user/{id}')
+async def userId(id: int):
+   return search_user(id)
+    
+#query    
+@app.get('/user/')
+async def userId(id: int):
+    return search_user(id)
+    
+def search_user(id):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {'error':'Usuario no existe'}
