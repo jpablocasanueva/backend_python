1969fd154e7c895ff673ae1f9813b874fba36e1e
jpcasanuevab <jpcasanuevab@outlook.com>
2025-04-01 00:15:16 -0300

    Creacion de Rutas en Main

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from  routers import products, users
 
 app = FastAPI()
+
+#routers
+app.include_router(products.router)
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
--- FastAPI/routers/users.py
+++ FastAPI/routers/users.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
-app = FastAPI()
+router = APIRouter()
 
 class User(BaseModel):
     id: int
@@ -14,35 +14,37 @@ users_list = [User(id=1,name= 'Bruno', surname= 'Casanueva', url= 'https://bruno
                 User(id=2,name= 'Luna', surname= 'Casanueva', url= 'https://luna.casanueva.com', age= 9),
                 User(id=3,name= 'Oso', surname= 'Casanueva', url= 'https://oso.casanueva.com', age= 3)]
 
-@app.get('/usersJson')
+@router.get('/usersJson')
 async def users():
     return  [{'name': 'Bruno', 'surname': 'Casanueva', 'url':'https://bruno.casanueva.com', 'age': 8},
              {'name': 'Luna', 'surname': 'Casanueva', 'url':'https://luna.casanueva.com', 'age': 9},
              {'name': 'Oso', 'surname': 'Casanueva', 'url':'https://oso.casanueva.com', 'age': 3}]
     
-@app.get('/userClass')
+@router.get('/userClass')
 async def usersClass():
     return users_list
 
 #Path
-@app.get('/user/{id}')
+@router.get('/user/{id}')
 async def userId(id: int):
    return search_user(id)
     
 #query    
-@app.get('/user/')
+@router.get('/user/')
 async def userId(id: int):
     return search_user(id)
     
     
-@app.post('/user/')
+@router.post('/user/',response_model=User,status_code=201)
 async def addUser(user: User):
     if type(search_user(user.id)) == User:
-        return {'error':'El usuario ya existe'}
-    else: 
-        users_list.append(user)
+        raise HTTPException(status_code=404, detail='El usuario ya existe')
+        # return {'error':}
+    
+    users_list.append(user)
+    return user
 
-@app.put('/user/')
+@router.put('/user/')
 async def updateUser(user: User):
     found = False
     
@@ -55,6 +57,19 @@ async def updateUser(user: User):
         return {'error': 'No se ha actualizado el usuario'}
     else:
         return user
+    
+@router.delete('/user/{id}')
+async def deleteUser(id: int):
+     found = False
+     
+     for index, saved_user in enumerate(users_list):
+         if saved_user.id  == id:
+             del users_list[index]
+             found = True
+     
+     if not found:
+         return {'error': 'No se ha eliminado el usuario'}
+                
 
 def search_user(id):
     users = filter(lambda user: user.id == id, users_list)
